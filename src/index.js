20d46a82b9c47e659f4a9bfa2267871960b24d5f
Sadek <39184773+Sadek2002@users.noreply.github.com>
2022-05-17 22:42:39 +0200

    Removed a class that isn't used

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { IntlProvider } from "react-intl";
 import Wrapper from "./components/Wrapper";
 
 ReactDOM.render(
